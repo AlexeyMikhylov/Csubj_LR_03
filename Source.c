99b1171e378f5be4fc7107763929d0068667b96e
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-04-28 23:32:11 +0300

    .1

--- Source.c
+++ Source.c
@@ -2,35 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     FILE* inputFile, * outputFile;
     char inputFileName[] = "input.txt";
     char outputFileName[] = "output.txt";
     char line[100];
 
     inputFile = fopen(inputFileName, "r");
-    if (inputFile == NULL) {
+    if (inputFile == NULL)
+    {
         perror("Error opening file");
         return 1;
     }
 
     outputFile = fopen(outputFileName, "w");
-    if (outputFile == NULL) {
+    if (outputFile == NULL)
+    {
         perror("Error opening file");
         return 1;
     }
 
-    while (fgets(line, sizeof(line), inputFile)) {
+    while (fgets(line, sizeof(line), inputFile))
+    {
         int maxDigit = -1;
         int length = strlen(line);
 
-        for (int i = 0; i < length; i++) {
-            if (line[i] >= '0' && line[i] <= '9' && (line[i] - '0') > maxDigit) {
+        for (int i = 0; i < length; i++)
+        {
+            if (line[i] >= '0' && line[i] <= '9' && (line[i] - '0') > maxDigit)
+            {
                 maxDigit = line[i] - '0';
             }
         }
 
-        fprintf(outputFile, "%s%d\n", line, maxDigit);
+        line[length - 1] = maxDigit + '0';
+        fprintf(outputFile, "%s\n", line);
     }
 
     fclose(inputFile);
